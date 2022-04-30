4ecd4d2c7ed5fe3c759d5177aaacc8bd6f0edbe7
wangchao <wangxiaochaocccc@163.com>
2022-04-30 22:59:24 +0800

    feat(component): svgicon组件封装

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <!-- 外部图标 -->
+  <div v-if="isExternal" class='svg-icon svg-external-icon' :style="externalIconStyle" :class="iconClassName"></div>
+  <!-- 内部图标 -->
+  <svg v-else class="svg-icon" :class="iconClassName">
+    <use :xlink:href="iconIn" />
+  </svg>
+</template>
+
+<script setup>
+import { defineProps, computed } from 'vue'
+import { isExternal as external } from '@/utils/validate.js'
+const props = defineProps({
+  // icon图标
+  icon: {
+    type: String,
+    required: true
+  },
+  // 图标类名
+  iconClassName: {
+    type: String,
+    default: ''
+  }
+})
+// 判断是否为外部图标
+const isExternal = computed(() => external(props.icon))
+
+// 外部图标样式
+const externalIconStyle = computed(() => ({
+  mask: `url(${props.icon}) no-repeat 50% 50%`,
+  '-webkit-mask': `url(${props.icon}) no-repeat 50% 50%`
+}))
+
+// 内部图标
+const iconIn = computed(() => `#icon-${props.icon}`)
+</script>
+<style lang='scss'>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+
+.svg-external-icon {
+  background-color: currentColor;
+  mask-size: cover !important;
+  display: inline-block;
+}
+</style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,38 +1,38 @@
 <template>
-    <div class='login-container'>
-      <el-form class="form-container">
-        <h3>用户登录</h3>
-        <el-form-item>
-          <span class="svg-container">
-            <el-icon>
-              <Avatar />
-            </el-icon>
-          </span>
-          <el-input placeholder="请输入用户名" name="username" type="text"></el-input>
-        </el-form-item>
+  <div class='login-container'>
+    <el-form class="form-container">
+      <h3>用户登录</h3>
+      <el-form-item>
+        <span class="svg-container">
+          <svg-icon icon="https://res.lgdsunday.club/user.svg"></svg-icon>
+        </span>
+        <el-input placeholder="请输入用户名" name="username" type="text"></el-input>
+      </el-form-item>
 
-        <el-form-item>
-          <span class="svg-container">
-            <el-icon>
-              <Avatar />
-            </el-icon>
-          </span>
-          <el-input placeholder="请输入密码" name="password"></el-input>
-          <span class="svg-password-show">
-            <el-icon>
-              <Avatar />
-            </el-icon>
-          </span>
-        </el-form-item>
-        <el-button type="primary" style="width: 100%; margin-bottom: 30px;">登录</el-button>
-      </el-form>
-    </div>
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon>
+            <Avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="请输入密码" name="password"></el-input>
+        <span class="svg-password-show">
+          <el-icon>
+            <Avatar />
+          </el-icon>
+        </span>
+      </el-form-item>
+      <el-button type="primary" style="width: 100%; margin-bottom: 30px;">登录</el-button>
+    </el-form>
+  </div>
 
 </template>
 
 <script>
+import SvgIcon from '../../components/SvgIcon/index.vue'
 export default {
-  name: 'login'
+  name: 'login',
+  components: { SvgIcon }
 }
 </script>
 <style lang='scss'>
@@ -40,12 +40,14 @@ export default {
   min-width: 100%;
   min-height: 100%;
   background-color: #2d3a4b;
+
   .form-container {
     position: relative;
     width: 520px;
     padding: 160px 35px 0;
     margin: 0 auto;
     overflow: hidden;
+
     h3 {
       text-align: center;
       color: #fff;
@@ -53,15 +55,17 @@ export default {
     }
 
     .el-form-item {
-      border: 1px solid rgba(255,255,255,.1);
-      background-color: rgba(0,0,0,.1);
+      border: 1px solid rgba(255, 255, 255, .1);
+      background-color: rgba(0, 0, 0, .1);
       border-radius: 5px;
       color: #454545;
+
       .svg-container {
         padding: 6px 5px 6px 15px;
         color: #889aa4;
         vertical-align: middle;
       }
+
       .svg-password-show {
         position: absolute;
         right: 10px;
@@ -71,16 +75,19 @@ export default {
         user-select: none;
       }
     }
-     .el-input {
+
+    .el-input {
       display: inline-block;
       height: 47px;
       width: 90%;
       border: none;
+
       .el-input__wrapper {
         width: 100%;
         background: transparent;
         box-shadow: none;
       }
+
       input {
         width: 100%;
         height: 47px;
