2e9066d3cf5e02fa9ca4837f9b5028256950dc0f
wangchao <wangxiaochaocccc@163.com>
2022-05-01 22:06:18 +0800

    feat: 封装请求动作

---
+++ src/store/modules/user.js
@@ -0,0 +1,19 @@
+import login from '@/api/sys'
+import md5 from 'md5'
+export default {
+  actions: {
+    login (context, userInfo) {
+      const { username, password } = userInfo
+      return new Promise((resolve, reject) => {
+        login({
+          username,
+          password: md5(password)
+        }).then(() => {
+          resolve()
+        }).catch(err => {
+          reject(err)
+        })
+      })
+    }
+  }
+}
