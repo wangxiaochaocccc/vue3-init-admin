5bc3db3190ea1389c21eeb627a85396059c64359
wangchao <wangxiaochaocccc@163.com>
2022-05-02 10:16:22 +0800

    feat(axios): 封装response

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { ElMessage } from 'element-plus'
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_URL,
@@ -10,6 +11,23 @@ service.interceptors.request.use(
   config => {
     config.headers.icode = 'F7CEF22D3DFD9445'
     return config
+  }
+)
+
+// 相应数据拦截
+service.interceptors.response.use(
+  response => {
+    const { data, success, message } = response.data
+    if (success) {
+      return data
+    } else {
+      ElMessage.error(message)
+      return Promise.reject(new Error(message))
+    }
+  },
+  error => {
+    ElMessage.error(error.message)
+    return Promise.reject(error)
   }
 )
 
