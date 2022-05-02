e1f6b1acefea5b34c389ecdad52a5a3ef63b4a3f
wangchao <wangxiaochaocccc@163.com>
2022-05-03 00:28:56 +0800

    feat(sidebar): 创建临时menu菜单

---
+++ src/layout/sidebar/SideBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+    <el-sub-menu index="1">
+      <template #title>
+        <el-icon>
+          <location />
+        </el-icon>
+        <span>选项一</span>
+      </template>
+      <el-menu-item>选项1-1</el-menu-item>
+      <el-menu-item>选项1-2</el-menu-item>
+    </el-sub-menu>
+    <el-sub-menu index="2">
+      <template #title>
+        <el-icon>
+          <location />
+        </el-icon>
+        <span>选项二</span>
+      </template>
+      <el-menu-item>选项1-1</el-menu-item>
+      <el-menu-item>选项1-2</el-menu-item>
+    </el-sub-menu>
+  </el-menu>
+</template>
+
+<script setup>
+
+</script>
+<style lang='scss'>
+</style>
--- src/layout/sidebar/index.vue
+++ src/layout/sidebar/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class=''>SideBar</div>
+  <div class="side-bar-container">
+    <h1>站位</h1>
+    <el-scrollbar>
+      <side-bar></side-bar>
+    </el-scrollbar>
+
+  </div>
 </template>
 
 <script setup>
-
+import SideBar from './SideBar.vue'
 </script>
 <style lang='scss'>
 </style>
