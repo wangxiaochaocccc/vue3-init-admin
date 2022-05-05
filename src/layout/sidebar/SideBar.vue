8819921c7e789c87f854be0ec4eb1bdc7ee69121
wangchao <wangxiaochaocccc@163.com>
2022-05-05 09:26:45 +0800

    feat(menu): 主题颜色

--- src/layout/sidebar/SideBar.vue
+++ src/layout/sidebar/SideBar.vue
@@ -1,5 +1,7 @@
 <template>
-  <el-menu default-active="2" class="el-menu-vertical-demo">
+  <el-menu default-active="2" class="el-menu-vertical-demo" :background-color="$store.getters.cssVar.menuBg"
+    :text-color="$store.getters.cssVar.menuText" :active-text-color="$store.getters.cssVar.menuActiveText"
+    :unique-opened="true">
     <side-bar-item v-for="item in routes" :key="item.path" :route="item"></side-bar-item>
   </el-menu>
 </template>
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,10 +1,13 @@
+import Variable from '@/assets/styles/variable.scss'
 
+// 快捷访问
 const getters = {
   token: state => state.user.token,
   hasUserInfo: state => {
     return JSON.stringify(state.user.userInfo) !== '{}'
   },
-  userInfo: state => state.user.userInfo
+  userInfo: state => state.user.userInfo,
+  cssVar: state => Variable
 }
 
 export default getters
