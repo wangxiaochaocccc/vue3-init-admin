820968d2de866405c78068f50795146080ca3737
wangchao <wangxiaochaocccc@163.com>
2022-05-05 17:25:13 +0800

    feat(breadcrumb): 面包屑动态渲染

--- src/components/breadcrumb/index.vue
+++ src/components/breadcrumb/index.vue
@@ -1,17 +1,48 @@
 <template>
   <el-breadcrumb class="breadcrumb" separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
-    <el-breadcrumb-item><a href="/">promotion management</a></el-breadcrumb-item>
-    <el-breadcrumb-item>promotion list</el-breadcrumb-item>
-    <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+    <el-breadcrumb-item v-for="(item, index) in breadcrumb" :key="item.path">
+      <span class="no-redirect" v-if="index === breadcrumb.length - 1">{{ item.meta.title }}</span>
+      <span class="redirect" v-else @click="lintTOHandle(item)">{{ item.meta.title }}</span>
+    </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
 <script setup>
+import { ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const breadcrumb = ref([])
+
+const getBreadCrumb = () => {
+  breadcrumb.value = route.matched.filter(item => item.meta?.title)
+}
+watch(
+  route,
+  () => {
+    getBreadCrumb()
+  }, { immediate: true }
+)
+
+// 跳转
+const router = useRouter()
+const lintTOHandle = item => {
+  router.push(item.path)
+}
 </script>
 <style lang='scss'>
 .breadcrumb {
   display: inline-block;
   font-size: 14px;
+
+  .redirect {
+    color: #666;
+    font-weight: bold;
+    cursor: pointer;
+  }
+
+  .no-redirect {
+    color: #97a8be;
+  }
 }
 </style>
