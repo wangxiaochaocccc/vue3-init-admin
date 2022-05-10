6649bd0f92de28c86452b0e0944726906269825d
wangchao <wangxiaochaocccc@163.com>
2022-05-10 12:23:53 +0800

    feat(国际化): element-plus的国际化

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './assets/styles/index.scss'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import installSvgIcon from './icons/index.js'
+import installElementPlus from './plugins/element'
 import i18n from './i18n'
 import './permission.js'
 const app = createApp(App)
@@ -15,4 +15,5 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 installSvgIcon(app)
-app.use(store).use(router).use(i18n).use(ElementPlus).mount('#app')
+installElementPlus(app)
+app.use(store).use(router).use(i18n).mount('#app')
