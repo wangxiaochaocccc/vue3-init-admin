fe2abaf019844dd39588a03875965a632505d5be
wangchao <wangxiaochaocccc@163.com>
2022-05-11 23:10:10 +0800

    fix(国际化): 默认值问题

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -4,7 +4,7 @@ export default {
   namespaced: true,
   state: () => ({
     sideBarOpened: true,
-    language: getItem(LANG) || ''
+    language: getItem(LANG) || 'zh'
   }),
   mutations: {
     triggerSideBarOpened (state) {
