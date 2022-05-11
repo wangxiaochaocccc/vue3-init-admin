c9d77f2f9ccfaf392be05aa947a1d693e72211c0
wangchao <wangxiaochaocccc@163.com>
2022-05-11 22:29:34 +0800

    fix(theme): 主题色刷新后无效问题

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
   <router-view />
 </template>
 <script setup>
-import { getOriginStyle } from '@/utils/theme.js'
-getOriginStyle()
+import { writeNewStyle, generateNewStyle } from '@/utils/theme.js'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+generateNewStyle(store.getters.mainColor).then(newStyle => {
+  writeNewStyle(newStyle)
+})
 </script>
 <style lang="scss">
 </style>
