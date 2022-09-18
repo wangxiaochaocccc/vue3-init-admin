6ea0ce610002114eab8c7583cb29f5112ca26bda
wangchao <wangxiaochaocccc@163.com>
2022-09-18 23:50:12 +0800

    feat(panthumb): panthumb组件

--- src/views/profile/components/ProjectCard.vue
+++ src/views/profile/components/ProjectCard.vue
@@ -1,8 +1,14 @@
 <template>
-    <div class=''>项目介绍</div>
+    <div class=''>
+        <pan-thumb image="/logo.jpeg" :height="'100px'" :width="'100px'">
+            <p><a>Hello</a></p>
+            {{ $store.getters.userInfo.title }}
+        </pan-thumb>
+    </div>
 </template>
 
 <script setup>
+import PanThumb from '@/components/PanThumb/index.vue'
 </script>
 <style lang='scss'>
 
