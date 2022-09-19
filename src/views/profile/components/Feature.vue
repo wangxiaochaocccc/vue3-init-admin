7afddb6522bdb8656a7df973cdecca08e07a6128
wangchao <wangxiaochaocccc@163.com>
2022-09-19 15:57:47 +0800

    feat(profile): 功能模块

--- src/views/profile/components/Feature.vue
+++ src/views/profile/components/Feature.vue
@@ -1,9 +1,26 @@
 <template>
-    <div class=''>特点</div>
+  <el-collapse v-model="activeName" accordion>
+    <el-collapse-item
+      v-for="item in featureData"
+      :key="item.id"
+      :name="item.id"
+      :title="item.title"
+    >
+      <div v-html="item.content"></div>
+    </el-collapse-item>
+  </el-collapse>
 </template>
 
 <script setup>
-</script>
-<style lang='scss'>
+import { ref } from 'vue'
 
-</style>
+defineProps({
+  featureData: {
+    type: Array,
+    required: true
+  }
+})
+
+const activeName = ref(0)
+</script>
+<style lang="scss"></style>
