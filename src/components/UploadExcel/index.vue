8f1759a86d434ad8027fd371c9af3e00fe511827
wangchao <wangxiaochaocccc@163.com>
2022-09-21 17:57:59 +0800

    feat(import): 使用xlsx对对局进行读取

--- src/components/UploadExcel/index.vue
+++ src/components/UploadExcel/index.vue
@@ -2,9 +2,17 @@
   <div class="upload-excel-container">
     <div class="button-upload">
       <div class="button-upload-container">
-        <el-button type="primary">{{ $t('msg.uploadExcel.upload') }}</el-button>
+        <el-button type="primary" :loading="loading" @click="handleUploadClick">
+          {{ $t('msg.uploadExcel.upload') }}
+        </el-button>
       </div>
-      <input type="file" accept=".xlsx,xls" class="input-upload" />
+      <input
+        ref="inputRef"
+        type="file"
+        accept=".xlsx,xls"
+        class="input-upload"
+        @change="handleInputChange"
+      />
       <div class="drop-upload">
         <p>{{ $t('msg.uploadExcel.drop') }}</p>
       </div>
@@ -12,7 +20,78 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { read, utils } from 'xlsx'
+import { getHeaderRow } from './utils'
+
+const props = defineProps({
+  beforeUpload: Function,
+  onSuccess: Function
+})
+
+const loading = ref(false)
+// 点击上传
+const inputRef = ref(null)
+const handleUploadClick = () => {
+  inputRef.value.click()
+}
+// 选择文件后
+const handleInputChange = (e) => {
+  const files = e.target.files
+  const rawFile = files[0]
+  if (!rawFile) return
+  upload(rawFile)
+}
+// 上传事件
+const upload = (rawFile) => {
+  inputRef.value.value = null
+  // 如果没有指定上传前回调的话
+  if (!props.beforeUpload) {
+    readerData(rawFile)
+    return
+  }
+  // 如果指定了上传前回调，那么只有返回 true 才会执行后续操作
+  const before = props.beforeUpload(rawFile)
+  if (before) {
+    readerData(rawFile)
+  }
+}
+// 读取数据（异步）
+const readerData = (rawFile) => {
+  loading.value = true
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    // 在读取操作完成后触发
+    reader.onload = (e) => {
+      // 获取解析的数据
+      const data = e.target.result
+      // 利用xlsx对数据进行解析
+      const workbook = read(data, { type: 'array' })
+      // 获取第一章表格名称
+      const firstSheetName = workbook.SheetNames[0]
+      // 只读取sheet1的数据
+      const worksheet = workbook.Sheets[firstSheetName]
+      // 解析表头数据
+      const header = getHeaderRow(worksheet)
+      // 解析数据体
+      const results = utils.sheet_to_json(worksheet)
+      // 传入解析之后的数据
+      generateData({ header, results })
+      // loading 处理
+      loading.value = false
+      // 异步完成
+      resolve()
+    }
+    // 启动读取指定的Blob或File
+    reader.readAsArrayBuffer(rawFile)
+  })
+}
+// 根据导入内容，生成数据
+const generateData = (data) => {
+  props.onSuccess?.(data)
+}
+</script>
 <style lang="scss">
 .upload-excel-container {
   margin-top: 100px;
--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    <upload-excel />
+    <upload-excel :onSuccess="onSuccess" />
   </div>
 </template>
 
 <script setup>
 import UploadExcel from '@/components/UploadExcel/index.vue'
+
+const onSuccess = (data) => {
+  console.log(data)
+}
 </script>
 
 <style lang="scss" scoped></style>
