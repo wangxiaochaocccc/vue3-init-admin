051d563104793fcc4e6f4c8eaf467b974a8bba98
wangchao <wangxiaochaocccc@163.com>
2022-09-26 11:51:09 +0800

    feat(permission): 权限列表构建

--- src/views/permission-list/index.vue
+++ src/views/permission-list/index.vue
@@ -1,10 +1,43 @@
 <template>
-  <div></div>
+  <div>
+    <el-card>
+      <el-table
+        :data="allPermission"
+        style="width: 100%"
+        row-key="id"
+        border
+        default-expand-all
+        :tree-props="{ children: 'children', hasChildren: 'hasChildren' }"
+      >
+        <el-table-column
+          prop="permissionName"
+          :label="$t('msg.permission.name')"
+        ></el-table-column>
+        <el-table-column
+          prop="permissionMark"
+          :label="$t('msg.permission.mark')"
+        ></el-table-column>
+        <el-table-column
+          prop="permissionDesc"
+          :label="$t('msg.permission.desc')"
+        ></el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { getPermissionList } from '@/api/permission'
+import { watchSwitchLanguage } from '@/utils/i18n'
 
+// 获取数据
+const allPermission = ref([])
+const getData = async () => {
+  allPermission.value = await getPermissionList()
+}
+getData()
+watchSwitchLanguage(getData)
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
