57e11e6feed2a52425aec5d402d4d8b2537cd4ae
wangchao <wangxiaochaocccc@163.com>
2022-10-31 17:43:09 +0800

    fix(国际化): 分页组件国际化奇幻问题修复

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <router-view />
+  <el-config-provider :locale="store.getters.language === 'en' ? en : zhCn">
+    <router-view />
+  </el-config-provider>
 </template>
 <script setup>
 import { writeNewStyle, generateNewStyle } from '@/utils/theme.js'
 import { useStore } from 'vuex'
 import { watchSwitchLanguage } from '@/utils/i18n'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import en from 'element-plus/lib/locale/lang/en'
 
 const store = useStore()
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,10 +1,5 @@
 import ElementPlus from 'element-plus'
-import store from '@/store'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import en from 'element-plus/lib/locale/lang/en'
 
 export default app => {
-  app.use(ElementPlus, {
-    locale: store.getters.language === 'en' ? en : zhCn
-  })
+  app.use(ElementPlus)
 }
